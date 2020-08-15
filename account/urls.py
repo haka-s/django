d553ecd45887cdf366b74392c15dc3a0940b7848
haka-s <santi20092014@outlook.es>
2020-08-15 05:05:29 -0300

    profile page/picture

--- account/urls.py
+++ account/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('products/', views.products, name='products'),
     path('user/', views.userPage, name='user-page'),
+    path('account/', views.accountSettings, name='account'),    
     path('customer/<str:pk_test>/', views.customer, name="customer"),
     path('create_order/<str:pk>/', views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
