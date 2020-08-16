86279b7ad60fde0b7851dee1fe650d29d916c5fc
haka-s <santi20092014@outlook.es>
2020-08-16 02:12:36 -0300

    reset password added

--- stuff/settings.py
+++ stuff/settings.py
@@ -128,3 +128,11 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'd8609431@gmail.com'
+EMAIL_HOST_PASSWORD = 'Santilomas12'
